922aee5b56062ba6d1fcce71d9337c60c3815be6
Porkchopsss <pokoshih@gmail.com>
2020-09-21 20:20:23 +1000

    update API

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default{ 
     getUsers:function(){    
 
-        return axios.get("https://randomuser.me/api/?results=200&nat=us");
+        return axios.get("https://randomuser.me/api/?results=200&nat=au");
     }
 };
